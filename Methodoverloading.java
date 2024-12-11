c7b6e425f289a1020f94119df9a4f394d3afe0d6
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-11 11:14:14 +0530

    Add files via upload

---
+++ Methodoverloading.java
@@ -0,0 +1,17 @@
+class Methodoverloading{
+    static void a()
+    {
+        System.out.println("Hello");
+    }
+    static void a(int b){
+        System.out.println("hello "+b);
+    }
+    static void a(int b,int c){
+        System.out.println("hello"+b+" "+c);
+    }
+    public static void main(String args[]){
+        a();
+        a(6);
+        a(4,5);
+    }
+}

a99715151e45c911e4032e7700871e36ee8caef6
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-11 14:58:26 +0530

    Add files via upload

---
+++ Paramaterisedconstructor.java
@@ -0,0 +1,19 @@
+class a{
+    int c;
+    String b;
+    a(int c,String b)
+    {
+        this.c=c;
+        this.b=b;
+    }
+    void display()
+    {
+        System.out.println(b+" "+c);
+    }
+}
+public class Paramaterisedconstructor{
+    public static void main(String args[]){
+        a s=new a(678,"zakeel");
+        s.display();
+    }
+}

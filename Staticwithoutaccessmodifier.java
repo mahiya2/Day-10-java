83feb50d992f3ef4f00d0e6d105e4790f59905b1
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-11 11:13:53 +0530

    Add files via upload

---
+++ Staticwithoutaccessmodifier.java
@@ -0,0 +1,9 @@
+class Staticwithoutaccessmodifier{
+    static String s(){
+        return "hello Students";
+    }
+    public static void main(String args[]){
+        String res=s();
+        System.out.println(res);
+    }
+}

6f52af52c2e22e048bf089844db99ce11095ee15
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-11 13:16:46 +0530

    Add files via upload

---
+++ Sumofnaturalnumbers.java
@@ -0,0 +1,12 @@
+class Sumofnaturalnumbers{
+    int sum(int n)
+    {
+        if(n==0)return 0;
+        return n+sum(n-1);
+    }
+    public static void main(String args[]){
+        Sumofnaturalnumbers ob=new Sumofnaturalnumbers();
+        int res=ob.sum(10);
+        System.out.println(res);
+    }
+}

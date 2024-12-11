7303240e1697307ff215587a1925d73be0078788
mahiya2 <fazeelamuskan12@gmail.com>
2024-12-11 11:05:49 +0530

    Add files via upload

---
+++ Staticwithnonvoid.java
@@ -0,0 +1,9 @@
+class Staticwithnonvoid{
+    public static String s(){
+        return "hello students";
+    }
+   public static void main(String args[]){
+    String res=s();
+    System.out.println(res);
+   }
+ }
